56abe8a96862380e678bcf69b6ebad76f6899b56
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-12-17 17:13:05 -0800

    Still tried to fix this.

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,8 +1,15 @@
 function burgerButton() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "nav-bar") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-bar";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  }
+}
+
+// ("#navbarNavDropdown");
+
+function dropDown() {
+  const el = document.getElementById("navbarDropdownMenuLink");
+  el.parentElement.classList.toggle("show");
 }
