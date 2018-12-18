900dc289aede415a8d452240cc90c667f8b161fd
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-12-17 22:30:50 -0800

    Made navbar, footer, other details responsive. It's almost done. To do: buttons, show different buttons for mobiles and laptops.      : More context and pictures.

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,15 +1,28 @@
-function burgerButton() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "nav-bar") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-bar";
-  }
-}
+// function burgerButton() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "nav-bar") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "nav-bar";
+//   }
+// }
 
 // ("#navbarNavDropdown");
 
-function dropDown() {
-  const el = document.getElementById("navbarDropdownMenuLink");
-  el.parentElement.classList.toggle("show");
+// function dropDown() {
+//   const el = document.getElementById("navbarDropdownMenuLink");
+//   el.parentElement.classList.toggle("show");
+// }
+
+function burgerButton() {
+  //   const burgerBtn = document.getElementsByClassName("burger-button");
+  const test = $(".burger-button")
+    .find("a")
+    .toggle("display");
+
+  const test2 = $(".nav-item")
+    .find("a")
+    .toggle("display");
+
+  //   console.log("this is what I got", test + test2);
 }
